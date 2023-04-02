d8c92e058e5dd3024d2ef607623d6e0a8bd8d6f0
Voitko Viacheslav <voitko-viacheslav-ivanovich1992@ukr.net>
2023-04-02 11:00:24 +0300

    add part 2 (for main and footer 1280,768)

--- shelter/src/pets/pets.js
+++ shelter/src/pets/pets.js
@@ -1,3 +1,5 @@
 import '/shelter/src/sass/pets.scss'; 
-import '/shelter/src/sass/footer.scss'; 
+// import '/shelter/src/sass/footer.scss'; 
+// import '/shelter/src/sass/width1280.scss';
+// import '/shelter/src/sass/width768.scss'; 
 console.log('hi PEts')
